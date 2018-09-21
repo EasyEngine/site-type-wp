1c2d1bac5b49c73da1efdb3d860b62ad5b804c33
Kirtan Gajjar <kirtangajjar95@gmail.com>
2018-09-21 15:00:16 +0530

    Change custom.conf to user.conf
    Signed-off-by: Kirtan Gajjar <kirtangajjar95@gmail.com>

--- src/WordPress.php
+++ src/WordPress.php
@@ -313,8 +313,8 @@ class WordPress extends EE_Site_Command {
 		$site_nginx_default_conf = $site_conf_dir . '/nginx/main.conf';
 		$site_php_ini            = $site_conf_dir . '/php-fpm/php.ini';
 		$server_name             = ( 'subdom' === $this->site_data['app_sub_type'] ) ? $this->site_data['site_url'] . ' *.' . $this->site_data['site_url'] : $this->site_data['site_url'];
-		$custom_conf_dest        = $site_conf_dir . '/nginx/custom/custom.conf';
-		$custom_conf_source      = SITE_WP_TEMPLATE_ROOT . '/config/nginx/custom.conf.mustache';
+		$custom_conf_dest        = $site_conf_dir . '/nginx/custom/user.conf';
+		$custom_conf_source      = SITE_WP_TEMPLATE_ROOT . '/config/nginx/user.conf.mustache';
 		$process_user            = posix_getpwuid( posix_geteuid() );
 
 		\EE::log( 'Creating WordPress site ' . $this->site_data['site_url'] );
