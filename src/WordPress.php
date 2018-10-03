06d577d390aa85549399946d26ca5859e9c5ccf6
sagarnasit <sagarnasit@gmail.com>
2018-10-03 11:00:38 +0530

    add WP_REDIS_MAXTTL constant

--- src/WordPress.php
+++ src/WordPress.php
@@ -276,19 +276,21 @@ class WordPress extends EE_Site_Command {
 	 * Enable page cache.
 	 */
 	private function enable_page_cache() {
-		$activate_nginx_helper    = 'docker-compose exec --user=\'www-data\' php wp plugin install nginx-helper --activate';
-		$nginx_helper_fail_msg    = 'Unable to download or activate nginx-helper plugin';
+		$activate_nginx_helper = 'docker-compose exec --user=\'www-data\' php wp plugin install nginx-helper --activate';
+		$nginx_helper_fail_msg = 'Unable to download or activate nginx-helper plugin';
 		$salt_value            = $this->site_data['site_url'] . ':';
 		$add_hostname_constant = "docker-compose exec --user='www-data' php wp config set RT_WP_NGINX_HELPER_REDIS_HOSTNAME ee-global-redis --add=true --type=constant";
 		$add_port_constant     = "docker-compose exec --user='www-data' php wp config set RT_WP_NGINX_HELPER_REDIS_PORT 6379 --add=true --type=constant";
 		$add_prefix_constant   = "docker-compose exec --user='www-data' php wp config set RT_WP_NGINX_HELPER_REDIS_PREFIX nginx-cache: --add=true --type=constant";
 		$add_cache_key_salt    = "docker-compose exec --user='www-data' php wp config set WP_CACHE_KEY_SALT $salt_value --add=true --type=constant";
+		$add_redis_maxttl      = "docker-compose exec --user='www-data' php wp config set WP_REDIS_MAXTTL 14400 --add=true --type=constant";
 
 		$this->docker_compose_exec( $add_hostname_constant, $nginx_helper_fail_msg );
 		$this->docker_compose_exec( $add_port_constant, $nginx_helper_fail_msg );
 		$this->docker_compose_exec( $add_prefix_constant, $nginx_helper_fail_msg );
 		$this->docker_compose_exec( $add_cache_key_salt, $nginx_helper_fail_msg );
 		$this->docker_compose_exec( $activate_nginx_helper, $nginx_helper_fail_msg );
+		$this->docker_compose_exec( $add_redis_maxttl, $nginx_helper_fail_msg );
 	}
 
 	/**
