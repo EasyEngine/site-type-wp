7542c863e5acd4d9d7a0448014032d340397f1fe
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-10-05 14:16:28 +0530

    Remove running cron immediately after site creation
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/WordPress.php
+++ src/WordPress.php
@@ -545,7 +545,6 @@ class WordPress extends EE_Site_Command {
 
 		\EE::log( 'Creating cron entry' );
 		\EE::runcommand( 'cron create ' . $this->site_data['site_url'] . ' --user=www-data --command=\'wp cron event run --due-now\' --schedule=\'@every 5m\'' );
-		\EE::exec( 'cd ' . $this->site_data['site_fs_path'] . ' && docker-compose exec php wp cron event run --due-now' );
 
 		$this->info( [ $this->site_data['site_url'] ], [] );
 		\EE::log( 'Site entry created.' );
