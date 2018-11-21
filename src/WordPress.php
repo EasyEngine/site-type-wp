7fc6fcd177791c235d8e501cd67e82c078ff25ff
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-11-21 18:21:36 +0530

    Update option and version logic
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/WordPress.php
+++ src/WordPress.php
@@ -114,6 +114,7 @@ class WordPress extends EE_Site_Command {
 	 * default: latest
 	 * options:
 	 * 	- 5.6
+	 *  - 7.2
 	 * 	- latest
 	 * ---
 	 *
@@ -230,16 +231,16 @@ class WordPress extends EE_Site_Command {
 		$supported_php_versions = [ 5.6, 7.2, 'latest' ];
 		if ( ! in_array( $this->site_data['php_version'], $supported_php_versions ) ) {
 			$old_version = $this->site_data['php_version'];
-			$yet         = '';
-			if ( 5 === (int) floor( $this->site_data['php_version'] ) ) {
+			$floor       = (int) floor( $this->site_data['php_version'] );
+			if ( 5 === $floor ) {
 				$this->site_data['php_version'] = 5.6;
-			} elseif ( 7 === (int) floor( $this->site_data['php_version'] ) || 'latest' === $this->site_data['php_version'] ) {
+			} elseif ( 7 === $floor ) {
 				$this->site_data['php_version'] = 7.2;
-				$yet = ' yet';
+				$old_version .= ' yet';
 			} else {
 				EE::error( 'Unsupported PHP version: ' . $this->site_data['php_version'] );
 			}
-			\EE::confirm( sprintf( 'EEv4 does not support PHP %s%s. Continue with PHP %s?', $old_version, $yet, $this->site_data['php_version'] ) );
+			\EE::confirm( sprintf( 'EEv4 does not support PHP %s. Continue with PHP %s?', $old_version, $this->site_data['php_version'] ) );
 		}
 
 		if ( \EE\Utils\get_flag_value( $assoc_args, 'local-db' ) ) {
