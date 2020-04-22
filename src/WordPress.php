b66373925ae17b3bc06492f26ca7329ebd412817
Riddhesh Sanghvi <riddhesh237@gmail.com>
2020-04-22 17:42:17 +0530

    Add wp-mail config option for default install mail
    Signed-off-by: Riddhesh Sanghvi <riddhesh237@gmail.com>

--- src/WordPress.php
+++ src/WordPress.php
@@ -345,7 +345,8 @@ class WordPress extends EE_Site_Command {
 			$this->site_data['db_port'] = empty( $arg_host_port[1] ) ? '3306' : $arg_host_port[1];
 		}
 
-		$this->site_data['app_admin_email'] = \EE\Utils\get_flag_value( $assoc_args, 'admin-email', strtolower( 'admin@' . $this->site_data['site_url'] ) );
+		$default_email                      = \EE::get_runner()->config['wp-mail'] ?? 'admin@' . $this->site_data['site_url'];
+		$this->site_data['app_admin_email'] = \EE\Utils\get_flag_value( $assoc_args, 'admin-email', strtolower( $default_email ) );
 		$this->skip_install                 = \EE\Utils\get_flag_value( $assoc_args, 'skip-install' );
 		$this->skip_status_check            = \EE\Utils\get_flag_value( $assoc_args, 'skip-status-check' );
 		$this->force                        = \EE\Utils\get_flag_value( $assoc_args, 'force' );
