e4b2fad84b06943cc7503a452544040de2b031f7
Kirtan Gajjar <kirtangajjar95@gmail.com>
2020-08-20 12:39:07 +0530

    Add json format in site info

--- src/WordPress.php
+++ src/WordPress.php
@@ -536,12 +536,26 @@ class WordPress extends EE_Site_Command {
 	 * [<site-name>]
 	 * : Name of the website whose info is required.
 	 *
+	 * [--format=<format>]
+	 * : Render output in a particular format.
+	 * ---
+	 * default: table
+	 * options:
+	 *   - table
+	 *   - csv
+	 *   - yaml
+	 *   - json
+	 *   - count
+	 *   - text
+	 * ---
+	 *
 	 * ## EXAMPLES
 	 *
 	 *     # Display site info
 	 *     $ ee site info example.com
 	 */
 	public function info( $args, $assoc_args ) {
+		$format   = \EE\Utils\get_flag_value( $assoc_args, 'format' );
 
 		\EE\Utils\delem_log( 'site info start' );
 		if ( ! isset( $this->site_data['site_url'] ) ) {
@@ -549,6 +563,14 @@ class WordPress extends EE_Site_Command {
 			$this->site_data  = get_site_info( $args, false );
 			$this->cache_type = $this->site_data['cache_nginx_fullpage'];
 		}
+
+		if ( 'json' === $format ) {
+			$site = (array) Site::find( $this->site_data['site_url'] );
+			$site = reset( $site );
+			EE::log( json_encode( $site ) );
+			exit();
+		}
+
 		$ssl    = $this->site_data['site_ssl'] ? 'Enabled' : 'Not Enabled';
 		$prefix = ( $this->site_data['site_ssl'] ) ? 'https://' : 'http://';
 		$info   = [ [ 'Site', $prefix . $this->site_data['site_url'] ] ];
